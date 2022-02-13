32adfae20e578b5b5cfd997082e3de94aae2f8dd
Vasilina-Zl <terisharkis@gmail.com>
2022-02-13 14:36:12 +0200

    first

---
+++ Address.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labor2_1
+{
+	public class Address
+	{
+		private int index;
+
+		public int Index
+		{
+			get { return index; }
+			set { index = value; }
+		}
+
+		private string country;
+
+		public string Country
+		{
+			get { return country; }
+			set { country = value; }
+		}
+
+		private string city;
+
+		public string City
+		{
+			get { return city; }
+			set { city = value; }
+		}
+
+		private string street;
+
+		public string Street
+		{
+			get { return street; }
+			set { street = value; }
+		}
+
+		private int house;
+
+		public int House
+		{
+			get { return house; }
+			set { house = value; }
+		}
+
+		private int apartment;
+
+		public int Apartment
+		{
+			get { return apartment; }
+			set { apartment = value; }
+		}
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Labor2_1.Address first = new Labor2_1.Address();
+
+		first.Index = 03217;
+		first.Country ="Ukraine";
+		first.City ="Kyiv";
+		first.Street ="Polova street";
+		first.House = 45;
+		first.Apartment = 3;
+
+		Console.WriteLine($"Index: {first.Index} | Country: {first.Country} |" +
+			$" City: {first.City} | Street: {first.Street} | House: {first.House} | Apartment: {first.Apartment}");
+
+	}
+}
